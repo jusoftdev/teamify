5553870c1daf9206b7030d3e0d1ee09a024c363d
juhom205 <80626044+juhom205@users.noreply.github.com>
2022-01-28 21:22:22 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDv7s0IiZ4r2pyPk5cjpbKxf-f9wqle1U4",
-  authDomain: "teamify-jusoft.firebaseapp.com",
-  projectId: "teamify-jusoft",
-  storageBucket: "teamify-jusoft.appspot.com",
-  messagingSenderId: "569215127271",
-  appId: "1:569215127271:web:e390b44d6986d1ebbe1c42",
-  measurementId: "G-64XNP34QPC"
+  // Firebase Config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
